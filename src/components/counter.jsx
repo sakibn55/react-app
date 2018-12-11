fd69ac773e42e7206c3b940a8d5f8f4c6bb19996
Sakib <nazmus.sakib@sjinnovation.com>
2018-12-11 21:53:48 +0600

    created new component and sends data via props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count:0,
+        value:this.props.value,
         imageUrl: 'https://picsum.photos/200',
         tags: ["tag1", "tag2","tag3"]
     };
@@ -18,13 +18,14 @@ class Counter extends Component {
     }
 
     handleIncrement = () =>{
-        console.log('clicked', this);
+        this.setState({ value: this.state.count + 1});
     }
 
     render() { 
-
+      
         return (
             <div className="container">
+            {this.props.children}
                 <div><img className="img-responsive" src={this.state.imageUrl} alt=""/></div>
                 <span style={ this.style } className={ this.getBandgeClass() }>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
@@ -34,13 +35,13 @@ class Counter extends Component {
     }
     getBandgeClass() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return count === 0 ? 'zero' : count;
+        const { value } = this.state;
+        return value === 0 ? 'zero' : value;
     }
 }
  
